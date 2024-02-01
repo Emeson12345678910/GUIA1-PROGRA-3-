9e112d79387c0a3c938bb50da27733c07b28751a
emerson <124007178+Emeson12345678910@users.noreply.github.com>
2024-01-31 21:53:30 -0600

    Add files via upload

---
+++ Guia1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guia1;
+
+/**
+ *
+ * @author MINEDUCYT
+ */
+public class Guia1 {
+    public static void main(String[] args) {
+        // Ingresar el nombre y apellidos directamente
+        String nombre = "TuNombre";
+        String apellidos = "TusApellidos";
+
+        // Concatenar el nombre y apellidos
+        String nombreCompleto = nombre + " " + apellidos;
+
+        // Imprimir el resultado
+        System.out.println("Tu nombre completo es: " + nombreCompleto);
+    }
+}
