493ee96667de8ac19be194da0f81ea762b83092e
emerson <124007178+Emeson12345678910@users.noreply.github.com>
2024-01-31 22:11:22 -0600

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio2;
+
+/**
+ *
+ * @author MINEDUCYT
+ */
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        double v1 = 1.5;
+        double v2 = 3.2;
+        double v3 = 2.0;
+        double suma = v1 + v2 + v3;
+        double resta = v1 - v2 - v3;
+        double multiplicacion = v1 * v2 * v3;
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicación: " + multiplicacion);
+    }
+}
